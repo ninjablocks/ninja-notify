967f6e7e2c63d538ecd0f168520cca955b9bdab7
☃ Elliot Shepherd <elliot@ninjablocks.com>
2014-04-28 16:09:48 +1000

    Cleanup, config and Nexmo SMS

--- index.js
+++ index.js
@@ -3,89 +3,146 @@
 var util = require('util');
 var stream = require('stream');
 
-var PushoverNotifier = require('./lib/PushoverNotifier');
-var PushbulletNotifier = require('./lib/PushbulletNotifier');
+var services = require('./lib/services');
 
 util.inherits(Driver,stream);
 
 function Driver(opts, app) {
+  this.log = app.log;
   this.app = app;
   this.opts = opts;
 
   app.once('client::up', this.init.bind(this));
 
-  this.opts.pushover = this.opts.pushover || [];
-  this.opts.pushbullet = this.opts.pushbullet || [];
-
+  this.opts.services = this.opts.services || {};
   this.devices = {};
 }
 
 Driver.prototype.init = function() {
-
-  this.opts.pushover.forEach(function(cfg) {
-    var device = new PushoverNotifier(this.app, this.opts.pushoverToken, cfg);
-    if (!this.devices[device.G]) {
-      this.emit('register', device);
-      device.emit('data', '');
-      this.devices[device.G] = device;
-    }
-
-  }.bind(this));
-
-  this.opts.pushbullet.forEach(function(cfg) {
-    var device = new PushbulletNotifier(this.app, cfg);
-    if (!this.devices[device.G]) {
-      this.emit('register', device);
-      device.emit('data', '');
-      this.devices[device.G] = device;
-    }
-  }.bind(this));
-
+  Object.keys(this.opts.services).forEach(this.initService.bind(this));
 };
 
+Driver.prototype.initService = function(id) {
+
+  var config = this.opts.services[id];
+
+  if (this.devices[config.id]) return;
+
+  this.log.info('Initialisng service', id, config);
+
+  var driver = this;
+
+  var service = services[config.service];
+
+  var device = new Device(config.service + config.id, config.name);
+  device.log = this.app.log;
+  device.write = function(data) {
+
+    if (typeof data === 'string') {
+      try {
+        data = JSON.parse(data);
+      } catch(e) {
+        data = {message: data};
+      }
+    }
+
+    this.log.debug('Notifying', config, 'with', data);
+
+    service.send(data, config, driver.app, function(err) {
+      if (err) {
+        driver.emit('announcement',{
+          "contents": [
+            { "type": "heading",      "text": "Failed to send notification" },
+            { "type": "paragraph",    "text": "There was an error sending the message '" + data.message + "' via notification service id : " + id}
+          ]
+        });
+      } else {
+        this.emit('data', data.message);
+      }
+    }.bind(this))
+  };
+
+  this.devices[id] = device;
+  this.emit('register', device);
+  device.emit('data', '');
+};
 
 Driver.prototype.config = function(rpc, cb) {
 
   if (!rpc) {
+
+    var options = Object.keys(services).map(function(id) {
+      return { 'type': 'submit', 'name': 'Add ' + services[id].name, 'rpc_method': id};
+    });
+
+    options.unshift({ "type": "heading", "text": "Add A Service" })
+
+    options.push({ "type": "heading", "text": "Remove A Service" })
+
+    if (Object.keys(this.opts.services).length) {
+      options.push({ 'type': 'submit', 'name': 'Open Current Services', 'rpc_method': 'delete'});
+    }
+
     return cb(null, {
-      'contents':[
-        { 'type': 'submit', 'name': 'Add Pushover Notifier', 'rpc_method': 'pushover' },
-        { 'type': 'submit', 'name': 'Add Pushbullet Notifier', 'rpc_method': 'pushbullet' }
-      ]
+      'contents': options
     });
   }
 
-  if (rpc.method == 'pushover') {
-    return cb(null, {
-      "contents":[
-        { "type": "paragraph", "text":"Please enter your Pushover details. You can register at http://pushover.net"},
-        { "type": "input_field_text", "field_name": "user", "value": "", "label": "User Key", "required": true},
-        { "type": "input_field_text", "field_name": "device", "value": "", "label": "Device (optional)", "required": false},
-        { "type": "submit", "name": "Save", "rpc_method": "pushoverSave" }
+  if (rpc.method === 'save') {
+    this.log.info('Saving new notification service', rpc.params);
+    
+    var config = rpc.params;
+    var service = services[config.service];
+    config.id = service.getId(config);
+    this.opts.services[config.id] = config;
+
+    this.save(this.opts);
+    this.initService(config.id);
+
+    this.emit('announcement',{
+      "contents": [
+        { "type": "heading",      "text": "Notification Service Saved" },
+        { "type": "paragraph",    "text": "'" + config.name + "' has been saved."}
       ]
     });
-  } else if (rpc.method == 'pushoverSave') {
-    this.opts.pushover.push(rpc.params);
-    this.save(this.opts);
-    this.init();
-    return cb(null);
-  } else if (rpc.method == 'pushbullet') {
+    cb(null);
+  } else if (rpc.method === 'delete') { 
+
+    var options = Object.keys(this.opts.services).map(function(id) {
+      return { 'type': 'submit', 'name': 'Delete ' + this.opts.services[id].name, 'rpc_method': 'delete-' + id};
+    }.bind(this));
+
+    options.unshift({ "type": "heading", "text": "Delete Notifications" });
+
     return cb(null, {
-      "contents":[
-        { "type": "paragraph", "text":"Please enter your Pushbullet details. You can register at https://www.pushbullet.com"},
-        { "type": "input_field_text", "field_name": "token", "value": "", "label": "API Key", "required": true},
-        { "type": "submit", "name": "Save", "rpc_method": "pushbulletSave" }
-      ]
+      'contents': options
     });
-  } else if (rpc.method == 'pushbulletSave') {
-    this.opts.pushbullet.push(rpc.params);
+  } else if (rpc.method.indexOf('delete') === 0) {
+    var id = rpc.method.split('-')[1];
+    this.log.info('Deleting notification id:', id);
+    delete(this.opts.services[id]);
     this.save(this.opts);
-    this.init();
-    return cb(null);
+    this.config({method:'delete'}, cb);
   } else {
-    return cb(true);
+
+    var service = services[rpc.method];
+    if (!service) {
+      return cb('Service ' + rpc.method + ' not found.');
+    }
+
+    cb(null, {"contents": service.config});
   }
 
 };
+
+function Device(id, name) {
+  this.G = ('notify'+id).replace(/[^a-z0-9]/ig, '');
+  this.V = 0;
+
+  this.D = 240; /* Text display */
+
+  this.name = name;
+}
+util.inherits(Device, stream);
 
 module.exports = Driver;
